09bb70a4807e33282001b5a706e8ecf109da7f2c
vpbmac <vitorpbarbosa7@gmail.com>
2024-11-11 16:45:00 -0300

    fix the add edge

--- course/psets/ps2/src/graph/UseConcreteEdgesGraph.java
+++ course/psets/ps2/src/graph/UseConcreteEdgesGraph.java
@@ -16,26 +16,32 @@ public class UseConcreteEdgesGraph {
 
         graph.set("A", "B", 4);
         graph.set("B", "D", 6);
-        System.out.println(graph.vertices());
-
+        graph.set("D", "E", 6);
         //print edges
         for (Edge edge: graph.getEdges()) {
             System.out.println(edge);
         }
+        // show vertices
+        System.out.println(graph.vertices());
+
         
         System.out.println("Targets");
         System.out.println(graph.targets("A"));
+        System.out.println(graph.targets("B"));
+        System.out.println("Sources");
+        System.out.println(graph.sources("B"));
+        System.out.println(graph.sources("D"));
 
 
-        // graph.remove("B");
-        // System.out.println("Remove B");
-        // System.out.println(graph.vertices());
+        graph.remove("B");
+        System.out.println("Remove B");
+        System.out.println(graph.vertices());
 
-        // System.out.println("Sources");
-        // System.out.println(graph.sources("B"));
+        System.out.println("Sources");
+        System.out.println(graph.sources("B"));
 
-        // System.out.println("Targets");
-        // System.out.println(graph.targets("A"));
+        System.out.println("Targets");
+        System.out.println(graph.targets("A"));
 
     }
     
