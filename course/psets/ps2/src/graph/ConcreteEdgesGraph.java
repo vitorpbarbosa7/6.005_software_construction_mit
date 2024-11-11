0d4cc90ce4f651d8ba0ad57c76d13f0e3643c909
vpbmac <vitorpbarbosa7@gmail.com>
2024-11-11 16:40:44 -0300

    fix the add edge

--- course/psets/ps2/src/graph/ConcreteEdgesGraph.java
+++ course/psets/ps2/src/graph/ConcreteEdgesGraph.java
@@ -6,6 +6,7 @@ package graph;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -40,12 +41,12 @@ public class ConcreteEdgesGraph implements Graph<String> {
     @Override public int set(String source, String target, int weight) {
 
         // construct local edge?
-        Edge localEdge = new Edge(source, target, weight);
+        Edge newEdge = new Edge(source, target, weight);
 
         for (Edge edge: edges) {
-            if (localEdge.similar(edge)) {
+            if (newEdge.similar(edge)) {
                 // if equal weight, return weight without doing nothing
-                if (edge.getWeight() == localEdge.getWeight()) {
+                if (edge.getWeight() == newEdge.getWeight()) {
                     return weight;
                 }
                 else {
@@ -53,27 +54,53 @@ public class ConcreteEdgesGraph implements Graph<String> {
                     int old_weight = edge.getWeight();
                     // must remove the old edge and add new edge
                     edges.remove(edge);
-                    edges.add(localEdge);
+                    edges.add(newEdge);
                     return old_weight;
                 }    
             }
         }
+
+        // if they do not exist in the vertices, add, and add the edge to the edge list
+        vertices.add(source);
+        vertices.add(target);
+        edges.add(newEdge);
         // if reaches here, so no similar edge was found, so return 0
         return 0;
     }
     
     @Override public boolean remove(String vertex) {
-        return vertices.remove(vertex);
+
+        // remove the vertex only
+        boolean removed = vertices.remove(vertex);
+        // now remove the edges with this vertex
+        if (removed){
+            Iterator<Edge> iterator = edges.iterator();
+            while(iterator.hasNext()){
+                Edge edge = iterator.next();
+                String localSource = edge.getSource();
+                String localTarget = edge.getTarget();
+                if (vertex.equals(localSource) || vertex.equals(localTarget)) {
+                    edges.remove(edge);
+                }
+            }
+        }
+        
+        return removed;
+        
     }
     
     @Override public Set<String> vertices() {
         return Collections.unmodifiableSet(this.vertices);
     }
+
+    public List<Edge> getEdges() {
+        return Collections.unmodifiableList(edges);
+    }
     
     @Override public Map<String, Integer> sources(String target) {
         Map<String, Integer> mapTarget = new HashMap<>();
         for (Edge edge: edges) {
-            if (edge.getTarget() == target) {
+            if (edge.getTarget().equals(target)) {
                 Integer localWeight = edge.getWeight();
                 String localSource = edge.getSource();
                 mapTarget.put(localSource, localWeight); 
@@ -86,7 +113,7 @@ public class ConcreteEdgesGraph implements Graph<String> {
     @Override public Map<String, Integer> targets(String source) {
         Map<String, Integer> mapSource = new HashMap<>();
         for (Edge edge: edges) {
-            if (edge.getSource() == source) {
+            if (edge.getSource().equals(source)) {
                 Integer localWeight = edge.getWeight();
                 String localTarget = edge.getTarget();
                 mapSource.put(localTarget, localWeight);
--- course/psets/ps2/src/graph/UseConcreteEdgesGraph.java
+++ course/psets/ps2/src/graph/UseConcreteEdgesGraph.java
@@ -8,7 +8,34 @@ public class UseConcreteEdgesGraph {
 
         graph.add("A");
         graph.add("B");
+        
+        System.out.println(graph.vertices());
 
+        graph.add("C");
+        System.out.println(graph.vertices());
+
+        graph.set("A", "B", 4);
+        graph.set("B", "D", 6);
+        System.out.println(graph.vertices());
+
+        //print edges
+        for (Edge edge: graph.getEdges()) {
+            System.out.println(edge);
+        }
+        
+        System.out.println("Targets");
+        System.out.println(graph.targets("A"));
+
+
+        // graph.remove("B");
+        // System.out.println("Remove B");
+        // System.out.println(graph.vertices());
+
+        // System.out.println("Sources");
+        // System.out.println(graph.sources("B"));
+
+        // System.out.println("Targets");
+        // System.out.println(graph.targets("A"));
 
     }
     
