3bc8777f019e83c3cbe29bc725d59e462048e939
vpbmac <vitorpbarbosa7@gmail.com>
2024-11-12 07:30:06 -0300

    ConcreteVertices start

--- course/psets/ps2/src/graph/ConcreteVerticesGraph.java
+++ course/psets/ps2/src/graph/ConcreteVerticesGraph.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.HashMap;
+import java.util.Collections;
 
 /**
  * An implementation of Graph.
@@ -66,21 +68,107 @@ public class ConcreteVerticesGraph implements Graph<String> {
  */
 class Vertex {
     
-    // TODO fields
+    // fields
+    private final String source;
+    private final HashMap<String, Integer> adjNodes;
     
     // Abstraction function:
-    //   TODO
+    //   AF(source, adjNodes):
+    //      Represents the connections of target vertices to the a source vertex, with their positive weights
+    //      Source vertex points to (directed) target vertices
     // Representation invariant:
-    //   TODO
+    //   source and adjNodes are final fields, so no reassignment is done, only mutable inside the adjNodes HashMap
     // Safety from rep exposure:
-    //   TODO
+    //   Client can't change 
+    //   adjNodes field is private, so no external access directly, only modifiable by the public methods
     
-    // TODO constructor
+    // constructor
+    public Vertex(String source, HashMap<String, Integer> adjNodes) {
+        this.source = source;
+        this.adjNodes = adjNodes;
+    }
     
-    // TODO checkRep
+    // checkRep
+    public void checkRep(){
+        assert source != null;
+        for (Map.Entry<String, Integer> entry : adjNodes.entrySet()){
+            assert entry.getKey() != null;
+            assert entry.getValue() != null;
+        }
+    }
     
-    // TODO methods
-    
+    // observer
+    public Map<String, Integer> getAdjNodes(){
+        Map<String, Integer> unmodifiableMap = Collections.unmodifiableMap(this.adjNodes);
+        return unmodifiableMap;
+    }
+
+    // observer
+    /**
+     *  
+     * @return The source node
+     */
+    public String getSource() {
+        return this.source;
+    }
+
+    // observer
+    /**
+     * Recevies the target and gets the Integer weight from a Target from the source node
+     * @param target
+     * @return weight from target 
+     */
+    public Integer getTargetWeight(String target) {
+        return adjNodes.get(target);
+    }
+
+    /**
+     * 
+     * @return The set of targets from source node
+     */
+    public Set<String> getTargets(){
+        Set<String> targets = adjNodes.keySet();
+        return targets;
+    }
+
+
+
+    // mutator
+    /**
+     *Set a new connection from source to the target, with weight 
+     * If weight is non positive, it will remove the connection from there
+     * @param target
+     * @param weight
+     * @return true if the value was changed, or false if the vajue did not exist there
+     */
+    public boolean set(String target, Integer weight){
+        if (weight <=0) {
+            return this.remove(target);
+        }
+        Integer previous_value = adjNodes.put(target, weight);
+        if (previous_value == null) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    // mutator
+    /**
+     *Remove a existing connection from source to target 
+     * @param target
+     * @return true if the value existed, or false if the value did not exist there
+     */
+    public boolean remove(String target){
+        Integer removed_value = adjNodes.remove(target);
+        if (removed_value == null){
+            return false;
+        } else {
+            return true;
+        }
+    }
+
     // TODO toString()
+
     
 }
