33bbe572a33603d41da431c6a11634c7de0323a0
vpbmac <vitorpbarbosa7@gmail.com>
2024-11-26 09:26:33 -0300

    convert Concrete Parse Tree to Abstract Syntax Tree

--- course/src/16_parsers/code/fa16-ex18-parser-generators/src/intexpr/MyMain.java
+++ course/src/16_parsers/code/fa16-ex18-parser-generators/src/intexpr/MyMain.java
@@ -78,5 +78,74 @@ public class MyMain {
         }
     }
 
+    /**
+     * Function converts a ParseTree to an IntegerExpression. 
+     * @param p
+     *  ParseTree<IntegerGrammar> that is assumed to have been constructed by the grammar in IntegerExpression.g
+     * @return
+     */
+    private static IntegerExpression buildAST(ParseTree<IntegerGrammar> p){
+
+        switch(p.getName()){
+        /*
+         * Since p is a ParseTree parameterized by the type IntegerGrammar, p.getName() 
+         * returns an instance of the IntegerGrammar enum. This allows the compiler to check
+         * that we have covered all the cases.
+         */
+        case NUMBER:
+            /*
+             * A number will be a terminal containing a number.
+             */
+            return new Number(Integer.parseInt(p.getContents()));
+        case PRIMITIVE:
+            /*
+             * A primitive will have either a number or a sum as child (in addition to some whitespace)
+             * By checking which one, we can determine which case we are in.
+             */             
+
+            if(p.childrenByName(IntegerGrammar.number).isEmpty()){
+                return buildAST(p.childrenByName(IntegerGrammar.sum).get(0));
+            }else{
+                return buildAST(p.childrenByName(IntegerGrammar.number).get(0));
+            }
+
+        case SUM:
+            /*
+             * A sum will have one or more children that need to be summed together.
+             * Note that we only care about the children that are primitive. There may also be 
+             * some whitespace children which we want to ignore.
+             */
+            boolean first = true;
+            IntegerExpression result = null;
+            for(ParseTree<IntegerGrammar> child : p.childrenByName(IntegerGrammar.PRIMITIVE)){                
+                if(first){
+                    result = buildAST(child);
+                    first = false;
+                }else{
+                    result = new Plus(result, buildAST(child));
+                }
+            }
+            if (first) {
+                throw new RuntimeException("sum must have a non whitespace child:" + p);
+            }
+            return result;
+        case ROOT:
+            /*
+             * The root has a single sum child, in addition to having potentially some whitespace.
+             */
+            return buildAST(p.childrenByName(IntegerGrammar.sum).get(0));
+        case WHITESPACE:
+            /*
+             * Since we are always avoiding calling buildAST with whitespace, 
+             * the code should never make it here. 
+             */
+            throw new RuntimeException("You should never reach here:" + p);
+        }   
+        /*
+         * The compiler should be smart enough to tell that this code is unreachable, but it isn't.
+         */
+        throw new RuntimeException("You should never reach here:" + p);
+    }
+
     
 }
